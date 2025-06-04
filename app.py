6960d9c982d133c5bff16b3d9466efd0129797fa
roma <monantoniet@gmail.com>
2025-06-04 14:23:13 +0200

    hem posat el port 5000 i obert laccess a tota la xarxa

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def admin():
     return render_template('admin.html', donacions=donacions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
